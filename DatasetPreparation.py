2d5f71dca9aac2808e3cb9fd043fd75f8b61a45b
guilherm.henrique <guilherme.amaral.pro@gmail.com>
2024-03-05 13:08:58 +0100

    First version

--- DatasetPreparation.py
+++ DatasetPreparation.py
@@ -2,18 +2,40 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
 from tqdm.notebook import tqdm
+import os.path
+from config import DATA_DIR, TEXT_FIELD_NAME
+
+
+csv_file_path = os.path.join(DATA_DIR, "./Data/RawData/steam.csv")
 
 model = SentenceTransformer(
     "all-MiniLM-L6-v2", device="cpu"
 )
 
-df = pd.read_json("./steam.json", orient="records")
+def calculate_embeddings(texts):
+    embeddings = model.encode(texts, show_progress_bar=False)
+    return embeddings
 
-vectors = model.encode(
-    [row.name+". " + row.categories for row in df.itertuples()],
-    show_progress_bar=True
-)
 
-print(vectors.shape)
+df = pd.read_csv(csv_file_path)
 
-np.save("steam.npy", vectors, allow_pickle=False)
+# Handle missing or non-string values in the TEXT_FIELD_NAME column
+df[TEXT_FIELD_NAME] = df[TEXT_FIELD_NAME].fillna('')  # Replace NaN with empty string
+df[TEXT_FIELD_NAME] = df[TEXT_FIELD_NAME].astype(str)  # Ensure all values are strings
+
+batch_size = 1000
+num_chunks = len(df) // batch_size + 1
+
+embeddings_list = []
+
+for i in tqdm(range(num_chunks), desc="Calculating Embeddings"):
+    start_idx = i * batch_size
+    end_idx = (i + 1) * batch_size
+    batch_texts = df[TEXT_FIELD_NAME].iloc[start_idx:end_idx].tolist()
+    batch_embeddings = calculate_embeddings(batch_texts)
+    embeddings_list.extend(batch_embeddings)
+
+# Convert embeddings list to a numpy array
+embeddings_array = np.array(embeddings_list)
+
+np.save("steam.npy", embeddings_array)
--- setupv2.py
+++ setupv2.py
@@ -3,55 +3,12 @@ import os.path
 import numpy as np
 import pandas as pd
 from qdrant_client import QdrantClient, models
-from tqdm import tqdm
-from sentence_transformers import SentenceTransformer
 
 from config import DATA_DIR, QDRANT_URL, COLLECTION_NAME, TEXT_FIELD_NAME, VECTOR_FIELD_NAME
 
 # Define the CSV file path and NPY file path
 csv_file_path = os.path.join(DATA_DIR, "./Data/RawData/steam.csv")
 npy_file_path = os.path.join(DATA_DIR, "steam.npy")
-
-"""
-def generate_embeddings():
-    # Load the SentenceTransformer model
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-
-    # Define a function to calculate embeddings
-    def calculate_embeddings(texts):
-        embeddings = model.encode(texts, show_progress_bar=False)
-        return embeddings
-
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv(csv_file_path)
-
-    # Handle missing or non-string values in the TEXT_FIELD_NAME column
-    df[TEXT_FIELD_NAME] = df[TEXT_FIELD_NAME].fillna('')  # Replace NaN with empty string
-    df[TEXT_FIELD_NAME] = df[TEXT_FIELD_NAME].astype(str)  # Ensure all values are strings
-
-    # Split the data into chunks to save RAM
-    batch_size = 1000
-    num_chunks = len(df) // batch_size + 1
-
-    embeddings_list = []
-
-    # Iterate over chunks and calculate embeddings
-    for i in tqdm(range(num_chunks), desc="Calculating Embeddings"):
-        start_idx = i * batch_size
-        end_idx = (i + 1) * batch_size
-        batch_texts = df[TEXT_FIELD_NAME].iloc[start_idx:end_idx].tolist()
-        batch_embeddings = calculate_embeddings(batch_texts)
-        embeddings_list.extend(batch_embeddings)
-
-    # Convert embeddings list to a numpy array
-    embeddings_array = np.array(embeddings_list)
-
-    # Save the embeddings to an NPY file
-    np.save(npy_file_path, embeddings_array)
-
-    print(f"Embeddings saved to {npy_file_path}")
-"""
-
 
 client = QdrantClient(
     "localhost",
