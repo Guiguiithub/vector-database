c1e3d2f1de0eee6c924bd569233d92a0bf6c5191
guilherm.henrique <guilherme.amaral.pro@gmail.com>
2024-03-20 15:13:18 +0100

    disabled buttons

--- static/getData.js
+++ static/getData.js
@@ -12,6 +12,13 @@ document.addEventListener('DOMContentLoaded', async function() {
     // Call the function to load initial games when the page loads
     loadInitialGames();
 });
+
+if (!localStorage.getItem('like')){
+    localStorage.setItem('like', JSON.stringify([]));
+}
+if (!localStorage.getItem('dislike')){
+    localStorage.setItem('dislike', JSON.stringify([]));
+}
 async function search() {
     const query = document.getElementById('searchInput').value.trim();
     if (query === '') {
@@ -37,7 +44,6 @@ async function search() {
 // Function to display search results
 function displayResults(results, number) {
 
-    console.log(results)
     const searchResults = document.getElementById('searchResults');
     searchResults.innerHTML = '';
 
@@ -53,8 +59,10 @@ function displayResults(results, number) {
                 <h5 class="card-title">${result.name}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${result.price}</h6>
                 <p class="card-text">${truncDescription(result.full_desc.desc)}</p>
-                <button class="btn btn-danger" onclick="addDislike('${result.ident}')">No</button>
-                <button class="btn btn-success" onclick="addLike('${result.ident}')">Like</button>
+                <div class="d-flex justify-content-between">
+                    <button class="btn btn-danger"  onclick="addDislike('${result.ident}')" style="visibility: `+findLike(result.ident)+`">👎</button>
+                    <button class="btn btn-success " onclick="addLike('${result.ident}')" style="visibility: `+findDislike(result.ident)+`">👍</button>
+                </div>
               </div>
             </div>
         `;
@@ -62,18 +70,54 @@ function displayResults(results, number) {
     });
 }
 
-let likeIds = []
-let dislikeIds = []
+function findLike(name){
+    let storageItem = JSON.parse(localStorage.getItem('like')) || [];
+    if (storageItem.includes(name)) {
+        return "hidden";
+    }
+    return "visible";
+}
+function findDislike(name){
+    let storageItem = JSON.parse(localStorage.getItem('dislike')) || [];
+    if (storageItem.includes(name)) {
+        return "hidden";
+    }
+    return "visible";
+}
 
 function addLike(name){
-    likeIds.push(name)
+    let likes = JSON.parse(localStorage.getItem('like')) || [];
+    if(!likes.includes(name)){
+        likes.push(name);
+    }
+    else {
+        likes = likes.filter(item => item !== name);
+    }
+    localStorage.setItem('like', JSON.stringify(likes));
+
+    updateButtonVisibility(name, 'like', !likes.includes(name));
 }
 
 function addDislike(name){
-    dislikeIds.push(name)
+    let dislikes = JSON.parse(localStorage.getItem('dislike')) || [];
+    if(!dislikes.includes(name)){
+        dislikes.push(name);
+    }
+    else {
+        dislikes = dislikes.filter(item => item !== name);
+    }
+    localStorage.setItem('dislike', JSON.stringify(dislikes));
+
+    updateButtonVisibility(name, 'dislike', !dislikes.includes(name));
 }
 
-async function recommand(){
+function updateButtonVisibility(name, type, visible) {
+    const buttons = document.querySelectorAll(`[data-name="${name}"][data-type="${type}"]`);
+    buttons.forEach(button => {
+        button.style.visibility = visible ? 'visible' : 'hidden'; // Set the visibility based on the 'visible' parameter
+    });
+}
+async function recommend(){
     const response = await fetch("http://localhost:8000/api/recommend", {
         method: 'POST',
         headers:{
